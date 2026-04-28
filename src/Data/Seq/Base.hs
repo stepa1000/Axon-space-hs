a805ae80a00910600e2e43175ab96d1899f1c989
stepa1000 <39490441+stepa1000@users.noreply.github.com>
2026-04-28 18:21:00 +0600

    init Seq base

---
+++ src/Data/Seq/Base.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Data.Axon.Base.Types where
+
+import Prelude as P
+
+import Control.Monad.STM
+import Control.Concurrent.STM.TVar
+import Control.Concurrent.STM.TArray
+import Control.Core.Composition
+import Control.Base.Comonad
+import Graphics.Gloss.Data.Picture
+import Graphics.Gloss.Data.Color
+import Data.Ix
+import Data.Functor.Adjunction
+import Control.Comonad
+import Control.Comonad.Env
+import Control.Monad.Reader
+import Control.Comonad.Trans.Adjoint as W
+import Data.Array.MArray
+import Debug.Trace
+import Control.Lens
+import System.Random
+import Data.Map as Map
+import Data.HashMap.Lazy as HMap
+import Data.Set as Set
+import Data.HashSet as HSet
+import Control.Concurrent.Async
+import Data.Traversable
+import Data.Foldable as Fold
+import Data.Proxy
+import Data.UUID
+import Data.Sequence as Seq
+import Data.Monoid
+import Data.Hashable
+
+import Data.Axon.Base.Types
